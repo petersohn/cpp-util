32ba0f776bf50a811a461e4ffdf9b14c9c567d0d
eszabpt <peter.szabados@ericsson.com>
2014-09-25 12:47:12 +0200

    Add Finally

---
+++ ut/finallyTest.cpp
@@ -0,0 +1,35 @@
+#include <boost/test/unit_test.hpp>
+#include "Finally.hpp"
+
+BOOST_AUTO_TEST_SUITE( finallyTest )
+
+struct FinallyTester {
+	bool called = false;
+
+	void operator()() { called = true; }
+};
+
+BOOST_AUTO_TEST_CASE(finally_should_call_function_at_end)
+{
+	FinallyTester finallyTester;
+	{
+		auto f = finally(std::ref(finallyTester));
+		BOOST_CHECK(!finallyTester.called);
+	}
+	BOOST_CHECK(finallyTester.called);
+}
+
+BOOST_AUTO_TEST_CASE(finally_should_call_function_at_end_even_if_exception_is_thrown)
+{
+	FinallyTester finallyTester;
+	try {
+		auto f = finally(std::ref(finallyTester));
+		throw 1;
+	} catch (...) {}
+	BOOST_CHECK(finallyTester.called);
+}
+
+BOOST_AUTO_TEST_SUITE_END() // demangleTest
+
+
+
