d65dac02657940ac66ae099078be98d82e10b0d4
Peter Szabados <kangirigungi@gmail.com>
2014-10-05 22:19:20 +0200

    Make LAZY_TYPE work from header

--- include/util/LazyType.hpp
+++ include/util/LazyType.hpp
@@ -7,8 +7,8 @@
 #define LAZY_TYPE(Type, name) \
 	using BOOST_PP_CAT(name, _type) = Type;\
 	BOOST_PP_CAT(name, _type) BOOST_PP_CAT(create_, name)();\
-	::std::unique_ptr<Type> BOOST_PP_CAT(name, _value);\
-	Type& name() { \
+	inline Type& name() { \
+		static ::std::unique_ptr<Type> BOOST_PP_CAT(name, _value);\
 		if (!BOOST_PP_CAT(name, _value)) { \
 			BOOST_PP_CAT(name, _value).reset(new Type(\
 					BOOST_PP_CAT(create_, name)())\
@@ -16,7 +16,7 @@
 		}\
 		return *BOOST_PP_CAT(name, _value);\
 	}\
-	Type BOOST_PP_CAT(create_, name)()
+	inline Type BOOST_PP_CAT(create_, name)()
 
 
 
