2bb2f25750c38b7a4def028aad3a322b0a1068db
Peter Szabados <kangirigungi@gmail.com>
2015-01-04 13:14:06 +0100

    Prepare PrefixMap to have multiple implementations

---
+++ include/util/PrefixMapException.hpp
@@ -0,0 +1,27 @@
+#ifndef INCLUDE_UTIL_PREFIXMAPEXCEPTION_HPP
+#define INCLUDE_UTIL_PREFIXMAPEXCEPTION_HPP
+
+#include <stdexcept>
+
+namespace util {
+
+struct PrefixMapError: std::logic_error {
+	using std::logic_error::logic_error;
+};
+
+struct AmbiguousValue: PrefixMapError {
+	AmbiguousValue(): PrefixMapError{"Ambiguous value"} {}
+};
+
+struct ValueNotFound: PrefixMapError {
+	ValueNotFound(): PrefixMapError{"Value not found"} {}
+};
+
+struct DuplicateValue: PrefixMapError {
+	DuplicateValue(): PrefixMapError{"Duplicate value or prefix of existing value"} {}
+};
+
+} // namespace util
+
+
+#endif /* INCLUDE_UTIL_PREFIXMAPEXCEPTION_HPP */
